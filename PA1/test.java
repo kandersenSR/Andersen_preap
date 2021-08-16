e8c3a1e09913f34887c37a11686d028127035229
Kurt Andersen <kandersen@sage.local>
2021-08-16 13:58:27 -0700

    first commit

---
+++ PA1/test.java
@@ -0,0 +1,13 @@
+public class test 
+{
+    public static void main (String[] args)
+    {
+        System.out.print("dog");
+        System.out.println("dog");
+        System.out.print("dog");
+    }
+}
+
+
+
+
